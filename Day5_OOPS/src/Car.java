eec5be6a17c1083a2d761c50b3d6338fbe902a6b
Nilotpal <Nilotpal@DESKTOP-9CN1MJ5>
2022-03-07 19:09:19 +0530

    1st changes 07/03/22

---
+++ Day5_OOPS/src/Car.java
@@ -0,0 +1,47 @@
+
+public class Car {
+	//member variables
+	//member functions/methods
+	
+	int no_of_cars;
+	String Model;
+	int speed;
+	
+	
+	public void setSpeed(int s) {
+		this.speed=s;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	
+	void display1() {
+		System.out.println("Welcoma to Audi Motors");
+	}
+	
+	void display2() {
+		System.out.println("Welcoma to Hyundai Motors");
+	}
+	
+	void display3() {
+		System.out.println("Welcoma to Kia-Motors");
+	}
+	
+	
+	public static void main(String args[]) {
+		
+		//object creation
+		
+		//syntax for object-- classname objectname==new classname();
+		
+		Car obj = new Car();
+		obj.display1();
+		obj.display2();
+		obj.display3();
+		
+		obj.setSpeed(90);
+		System.out.println(obj.getSpeed());
+		
+	}
+}
